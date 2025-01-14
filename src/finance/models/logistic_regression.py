cbb1c68a6a2613d1c4b71fc96e0b753635d13f6d
Felipe Giacomelli <fmw.giacomelli@gmail.com>
2025-01-13 22:25:48 -0300

    Type annotate code

--- src/finance/models/logistic_regression.py
+++ src/finance/models/logistic_regression.py
@@ -1,6 +1,7 @@
+# mypy: disallow-untyped-defs
 from sklearn.linear_model import LogisticRegression
 
 
-def get_logistic_regression_model(random_state=42):
+def get_logistic_regression_model(random_state: int = 42) -> LogisticRegression:
     """Return a Logistic Regression model."""
     return LogisticRegression(random_state=random_state)
